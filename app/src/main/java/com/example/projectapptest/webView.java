a2af20fe7f67168367a859686dc498bfa3427849
Nehal Patel <n9hal09@hotmail.com>
2021-04-09 15:30:20 +0530

    clickable card view

---
+++ app/src/main/java/com/example/projectapptest/webView.java
@@ -0,0 +1,61 @@
+package com.example.projectapptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class webView extends AppCompatActivity {
+
+    String url;
+
+    WebView webView;
+    @SuppressLint("RestrictedApi")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+        webView = findViewById(R.id.newsWeb);
+        getSupportActionBar().setDefaultDisplayHomeAsUpEnabled(true);
+
+        url =getIntent().getStringExtra("URL");
+        webView.loadUrl(url);
+
+        WebSettings settings =webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.setWebChromeClient(new WebChromeClient(){
+            @Override
+            public void onProgressChanged(WebView view, int newProgress) {
+                super.onProgressChanged(view, newProgress);
+            }
+
+            @Override
+            public void onReceivedTitle(WebView view, String title) {
+                super.onReceivedTitle(view, title);
+            }
+
+            @Override
+            public void onReceivedIcon(WebView view, Bitmap icon) {
+                super.onReceivedIcon(view, icon);
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        }
+        else{
+            finish();
+        }
+    }
+}
